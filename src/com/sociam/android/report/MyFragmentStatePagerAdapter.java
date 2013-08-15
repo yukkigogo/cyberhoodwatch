26f82d2358ad28a70aac5b05cdf347052e632683
yukkigogo <yukkigogo@gmail.com>
2013-08-15 10:54:57 +0100

    add files

---
+++ src/com/sociam/android/report/MyFragmentStatePagerAdapter.java
@@ -0,0 +1,38 @@
+package com.sociam.android.report;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+
+public class MyFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+
+	public MyFragmentStatePagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int i) {
+		// TODO Auto-generated method stub
+		switch(i){	
+		case 0: 
+			ReportFragment1 rp1 = new ReportFragment1();
+			return rp1;
+		case 1: 
+			ReportFragment2 rp2 = new ReportFragment2();
+			return rp2;
+		default:
+			return new ReportFragment1();
+		}
+		
+	}
+
+	@Override
+	public int getCount() {
+		return 2;
+	}
+	
+
+	
+}
