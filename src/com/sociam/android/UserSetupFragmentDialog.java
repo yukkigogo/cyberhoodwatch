325d60368a9c607726ab095078a1abead8e63cbb
yukkigogo <yukkigogo@gmail.com>
2013-11-19 12:22:06 +0000

    check the user avaiablity methods

--- src/com/sociam/android/UserSetupFragmentDialog.java
+++ src/com/sociam/android/UserSetupFragmentDialog.java
@@ -3,8 +3,11 @@ package com.sociam.android;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,9 +16,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class UserSetupFragmentDialog extends DialogFragment{
+	
+	SharedPreferences sp;
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		
+		sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
 		
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		View view = getActivity().getLayoutInflater().inflate(R.layout.register_user, null);
@@ -31,12 +39,12 @@ public class UserSetupFragmentDialog extends DialogFragment{
 		TextView tx4 = (TextView) view.findViewById(R.id.reg_text4);
 		tx4.setTypeface(robothin);
 		
+		final TextView tx_reg_ave = (TextView) view.findViewById(R.id.reg_avaiablity);
+		tx_reg_ave.setTypeface(robothin);
 		
-		
-		EditText username = (EditText) view.findViewById(R.id.reg_username);
+		final EditText username = (EditText) view.findViewById(R.id.reg_username);
 		username.setTypeface(robothin);
 		username.addTextChangedListener(new TextWatcher() {
-			
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 			
@@ -50,10 +58,34 @@ public class UserSetupFragmentDialog extends DialogFragment{
 			@Override
 			public void afterTextChanged(Editable s) {
 				
-				//KOKOKARA TMR
-				//UserAvaiableAsyncTask asyncTask = new UserAvaiableAsyncTask();
+				if(s.length()>0){
+				UserAvaiableAsyncTask asyncTask = new UserAvaiableAsyncTask(new FragmentUserSetupCallBack() {
+					
+					@Override
+					public void onTaskDone(int avaiable) {
+						if(avaiable==1){
+							// okay
+							tx_reg_ave.setTextColor(Color.GREEN);
+							tx_reg_ave.setText("Avaiable Username");
+							
+						}else if(avaiable==0){
+							tx_reg_ave.setTextColor(Color.RED);
+							tx_reg_ave.setText("This username is already taken");
+						}else{
+							tx_reg_ave.setText("");
+						}
+					}
+				});
+			
+				asyncTask.execute(username.getText().toString());
 				
+				
+			}else{
+				tx_reg_ave.setText("");
 			}
+			
+				
+			}	
 		});
 		
 		
